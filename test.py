f1364f314b80d1b81967fc596a8475f505acbec2
SmolHydra <80788935+SmolHydra@users.noreply.github.com>
2023-08-13 16:23:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,10 @@ print("Hello world")
 def greet(name):
   print("Hello",name)
 greet("Rukia")
+
+def multiply(num1,num2):
+  return num1*num2
+num1 = int(input("Enter a integer: "))
+num2 = int(input("Enter another integer: "))
+result = multiply(num1,num2)
+print(f"{num1} x {num2} = {result}")
