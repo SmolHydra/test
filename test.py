0605b7d8c243167acfeec35bd7b192e9bdbf4140
SmolHydra <80788935+SmolHydra@users.noreply.github.com>
2023-08-14 17:42:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print("Hello world")
 def greet(name):
   print("Hello",name)
-greet("Rukia")  #from bleach
+greet("Rukia")
 
 def multiply(num1,num2):
   return num1*num2
@@ -9,3 +9,8 @@ num1 = int(input("Enter a integer: "))
 num2 = int(input("Enter another integer: "))
 result = multiply(num1,num2)
 print(f"{num1} x {num2} = {result}")
+
+def gen_pw(fname,lname,dob,punc):
+  pw = lname[0]+fname+str(dob)+punc
+  return pw
+print(gen_pw("Rukia","Kuchiki",1857,"?"))
